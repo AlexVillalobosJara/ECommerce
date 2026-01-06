53b804c6cced0b9d4b9f3a279effba0a9005275a
AlexVillalobosJara <arvillalobos@gmail.com>
2026-01-06 20:12:28 -0300

    Fix 500 error: add missing Q import in payment_views.py

--- backend/orders/payment_views.py
+++ backend/orders/payment_views.py
@@ -8,6 +8,7 @@ from django.shortcuts import get_object_or_404
 from django.http import HttpResponseRedirect
 from django.utils import timezone
 from django.db import transaction
+from django.db.models import Q
 import logging
 import traceback
 
